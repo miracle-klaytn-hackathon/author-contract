80e2aa37fd53e1cd100cdfdd7a514c9a0f4b34c6
vanduc2514 <vanduc2514@gmail.com>
2023-08-20 19:14:19 +0700

    add AuthorContract

---
+++ src/main/resources/AuthorContract.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@klaytn/contracts/KIP/token/KIP7/KIP7.sol";
+import "@klaytn/contracts/access/Ownable.sol";
+
+contract AuthorContract is KIP7, Ownable {
+    constructor() KIP7("Author Token", "ATK") {
+        _mint(msg.sender, 1111000 * 10 ** decimals());
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+    public
+    view
+    virtual
+    override
+    returns (bool)
+    {
+        return
+            super.supportsInterface(interfaceId);
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
