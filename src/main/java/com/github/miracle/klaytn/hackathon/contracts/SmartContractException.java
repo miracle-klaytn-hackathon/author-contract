099fdb36b1d1ba50a87e854b70aed052b53156fa
fourtwenty <52255055+vanduc2514@users.noreply.github.com>
2023-08-20 19:36:04 +0700

    Issue #1 Create and deploy Author Token Smart Contract (#7)
    * Add Wrapper for Author Contract
    * Deploy Author Contract to testnet

---
+++ src/main/java/com/github/miracle/klaytn/hackathon/contracts/SmartContractException.java
@@ -0,0 +1,8 @@
+package com.github.miracle.klaytn.hackathon.contracts;
+
+public class SmartContractException extends Throwable {
+    public SmartContractException(String message) {
+        super(message);
+    }
+
+}
