aef37dac2d858eab25db2cfe9434f60ee60e00dd
vanduc2514 <vanduc2514@gmail.com>
2023-09-10 19:11:21 +0700

    fetch token from backend

--- src/main/java/com/github/miracle/klaytn/hackathon/utils/mappers/BookContractMapper.java
+++ src/main/java/com/github/miracle/klaytn/hackathon/utils/mappers/BookContractMapper.java
@@ -2,7 +2,9 @@ package com.github.miracle.klaytn.hackathon.utils.mappers;
 
 import java.util.List;
 
+import org.bson.types.ObjectId;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 
 import com.github.miracle.klaytn.hackathon.entities.BookContract;
@@ -11,6 +13,11 @@ import com.github.miracle.klaytn.hackathon.openapi.model.SmartContract;
 @Mapper(componentModel = MappingConstants.ComponentModel.CDI)
 public interface BookContractMapper {
 
+    default String toId(ObjectId objectId) {
+        return objectId.toHexString();
+    }
+
+    @Mapping(source = "id", target = "id", qualifiedByName = "toId")
     List<SmartContract> toSmartContract(List<BookContract> recommendations);
 
 }
