c996230bc1f8c10f59484dc44dd68dea3b2484d0
vanduc2514 <vanduc2514@gmail.com>
2023-11-26 13:31:18 +0700

    add api definition for user profile

---
+++ src/main/java/com/github/miracle/klaytn/hackathon/api/UserProfileResource.java
@@ -0,0 +1,36 @@
+package com.github.miracle.klaytn.hackathon.api;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+
+import com.github.miracle.klaytn.hackathon.openapi.api.UserProfileApi;
+import com.github.miracle.klaytn.hackathon.openapi.model.UserProfile;
+
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.SecurityContext;
+
+@ApplicationScoped
+public class UserProfileResource implements UserProfileApi {
+
+    @Context
+    private SecurityContext securityContext;
+
+    @Override
+    @RolesAllowed({"user", "author", "admin"})
+    public CompletionStage<Response> createUserProfile(UserProfile userProfile) {
+        throw new UnsupportedOperationException("Unimplemented method 'createUserProfile'");
+    }
+
+    @Override
+    @RolesAllowed({"user", "author", "admin"})
+    public CompletionStage<Response> getUserProfile(String address) {
+        if (!securityContext.getUserPrincipal().getName().equals(address)) {
+            return CompletableFuture.completedStage(Response.status(401).build());
+        }
+        throw new UnsupportedOperationException("Unimplemented method 'getProfile'");
+    }
+
+}
